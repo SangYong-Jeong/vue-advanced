6d02c4bd4265ece48af496145188ad44b6ef91be
SangYong-Jeong <mmaf@naver.com>
2022-01-13 22:33:22 +0900

    today 학습 vuex

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,21 +1,33 @@
 <template>
   <div>
-    ask
+    <p v-for="item in fetchedAsk">
+      <a v-bind:href="item.url">
+        {{ item.title }}
+      </a>
+      <small>{{item.time_ago}} by {{item.user}}</small>
+    </p>
   </div>
 </template>
 
 <script>
-import { fetchAskList } from '../api/index.js'
-
+import { mapGetters } from 'vuex'
 export default {
-  created() {
-    fetchAskList()
-      .then(function(response) {
-        console.log(response)
-      })
-      .catch(function(error) {
-        console.log(error)
-      })
+  computed: {
+    ...mapGetters([
+      'fetchedAsk'
+    ]),
+    // # 2
+    // ...mapState({
+    //   ask: state => state.ask
+    // })
+
+    //# 1
+    // ask () {
+    //   return this.$store.state.ask
+    // }
+  },
+  created () {
+    this.$store.dispatch('FETCH_ASK')
   }
 }
 </script>
--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,21 +1,19 @@
 <template>
   <div>
-    jobs
+    <p v-for="job in this.$store.state.jobs">
+      <a :href="job.url">
+        {{ job.title }}
+      </a>
+      <small>{{job.time_ago}} , {{ job.domain }}</small>
+    </p>
   </div>
 </template>
 
 <script>
-import { fetchJobsList } from '../api/index.js'
 
 export default {
   created() {
-    fetchJobsList()
-      .then(function(response) {
-        console.log(response)
-      })
-      .catch(function(error) {
-        console.log(error)
-      })
+    this.$store.dispatch('FETCH_JOBS')
   }
 }
 </script>
